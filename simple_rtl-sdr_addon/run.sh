3fa8f8d391f057d7eb39d8980fc4e2d424948fd7
adamaze <adamaze@gmail.com>
2021-07-06 11:59:33 -0400

    adding line to script

--- simple_rtl-sdr_addon/run.sh
+++ simple_rtl-sdr_addon/run.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/with-contenv bashio
+my_var=$(bashio::config 'my_var')
 
 echo "Hello world!"
 echo "this is a test addon made by Adam"
+echo the value of my_var from the config section is: $my_var
