f8aece685d347468a253332a09d990a51781538a
adamaze <adamaze@gmail.com>
2021-07-06 23:10:26 -0400

    fix script

--- simple_rtl-sdr_addon/run.sh
+++ simple_rtl-sdr_addon/run.sh
@@ -4,6 +4,6 @@ my_var=$(bashio::config 'my_var')
 echo "Hello world!"
 echo "this is a test addon made by Adam"
 echo the value of my_var from the config section is: $my_var
-ls -la /
 cd /
+ls -la
 ./rtlamr -filterid=34419315 -format=json -server=cenpi:1234
