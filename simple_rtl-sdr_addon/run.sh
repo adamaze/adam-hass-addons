1a4f9d38ff4ad67b98e829d8d6339d5d7eef135b
adamaze <adamaze@gmail.com>
2021-07-09 09:52:06 -0400

    removing sample stuff

--- simple_rtl-sdr_addon/run.sh
+++ simple_rtl-sdr_addon/run.sh
@@ -1,13 +1,8 @@
 #!/usr/bin/with-contenv bashio
-my_var=$(bashio::config 'my_var')
 server=$(bashio::config 'server')
 port=$(bashio::config 'port')
 serial_number=$(bashio::config 'serial_number')
-
-
-echo "Hello world!"
-echo "this is a test addon made by Adam"
-echo the value of my_var from the config section is: $my_var
-cd /
-ls -la
+#
+#
+echo "using rtlamr to connect to $server:$port listening for data from $serial_number"
 /root/go/bin/rtlamr -filterid=$serial_number -format=json -server=$server:$port
