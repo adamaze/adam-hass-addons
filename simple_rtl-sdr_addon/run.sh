3558c7537873d0d68d76edc0d07cb31b298bfbdf
adamaze <adamaze@gmail.com>
2021-07-09 09:46:41 -0400

    change script to use vars

--- simple_rtl-sdr_addon/run.sh
+++ simple_rtl-sdr_addon/run.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/with-contenv bashio
 my_var=$(bashio::config 'my_var')
+server=$(bashio::config 'server')
+port=$(bashio::config 'port')
+serial_number=$(bashio::config 'serial_number')
+
 
 echo "Hello world!"
 echo "this is a test addon made by Adam"
 echo the value of my_var from the config section is: $my_var
 cd /
 ls -la
-./rtlamr -filterid=34419315 -format=json -server=cenpi:1234
+/root/go/bin/rtlamr -filterid=$serial_number -format=json -server=$server:$port
